6760a4dadffa929c86b85d30fbac5da591b6cd7e
Selocan <sagilkaya26@gmail.com>
2021-03-17 16:29:17 +0300

    add some changes

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
-const panels = document.querySelectorAll('.panel')
+const panels = document.querySelectorAll('.panel');
 
-panels.forEach((panel)=>{
-    panel.addEventListener('click', ()=> {
-        removeActiveClasses()
-        panel.classList.add('active')
-    })
-})
+panels.forEach((panel) => {
+  panel.addEventListener('click', () => {
+    removeActiveClasses();
+    panel.classList.add('active');
+  });
+});
 
-function removeActiveClasses(){
-    panels.forEach(panel=> {
-        panel.classList.remove('active')
+function removeActiveClasses() {
+  panels.forEach((panel) => {
+    panel.classList.remove('active');
+  });
+}
 
-    })
+const forms = document.querySelectorAll('.order-form');
+
+forms.forEach((form) => {
+  form.addEventListener('click', () => {});
+  removeActiveClasses();
+  form.classList.add('active');
+});
+
+function removeActiveForm() {
+  forms.forEach((form) => {
+    form.classList.remove('active');
+  });
 }
